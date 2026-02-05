fab4754fb40cd40ac2022d9299b51e402ccf6495
BaekSeungCheol <kyn04241@gmail.com>
2026-02-05 18:14:45 +0900

    02.05 파이썬

--- basic/basic.py
+++ basic/basic.py
@@ -1,2 +1,15 @@
 # 주석
 print("파이썬 프로그래밍")
+
+str1 = "Life is too short, You need Python"
+str2 = "Life is too short, You need Python"
+str3 = """Life is too short, You need Python"""
+str4 = """Life is too short, You need Python"""
+
+print(str1)
+
+
+str1 = """Life is too short,\
+You need Python"""
+
+print(str1)
